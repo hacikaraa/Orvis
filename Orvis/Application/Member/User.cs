66b1e84c2f1c46eac3284e42d6bee4e6ad086793
murat-celik <muratcelik.27@outlook.com>
2015-11-12 20:01:35 +0000

    Member
    Member User enum

---
+++ Orvis/Application/Member/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orvis.Application.Member
+{
+    public class User : Base.Entity
+    {
+        public User()
+        {
+
+        }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Gender Gender { get; set; }//0=Bay--1=Bayan
+        public string Photo { get; set; }
+
+        public override void Configuration()
+        {
+            
+        }
+    }
+}
---
+++ Orvis/Application/Member/_Enums.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orvis.Application.Member
+{
+    public enum Gender
+    {
+        Man = 0,
+        Woman = 1
+    }
+}
